a771370ca11f2ce0e21821e3ddb1205b56c78216
UsamaBinAhmed6699 <usama.bin.ahmed6699@gmail.com>
2022-09-03 14:03:00 -0700

    adding the dummy directive

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,16 +2,21 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { BaseService } from './services/base.service';
+import { CurrencyDirectiveDirective } from './directives/currency-directive.directive';
 
 
 
 @NgModule({
   declarations: [
 
+  
+    CurrencyDirectiveDirective
   ],
   imports: [
     CommonModule, RouterModule  ],
-    exports:[],
+    exports:[
+    CurrencyDirectiveDirective
+  ],
     providers: [BaseService]
 })
 export class CoreModule { 
---
+++ src/app/core/directives/currency-directive.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCurrencyDirective]'
+})
+export class CurrencyDirectiveDirective {
+
+  constructor() { }
+
+}
